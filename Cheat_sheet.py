4d01209de63c0cd3f3d292590e8fc80344975133
jhmarlow1 <jhmarlow1@gmail.com>
2019-06-19 21:35:10 +0100

    Started creating class for chess game

--- Cheat_sheet.py
+++ Cheat_sheet.py
@@ -1,3 +1,17 @@
 # Install a pip package in the current Jupyter kernel
 import sys
 !{sys.executable} -m pip install <package>
+
+# Multiline editing
+Hold option
+
+# Quick docs
+select func, etc.
+Hold Option
+
+# Refactor all
+select
+press F2
+# Refactor file
+select
+press Ctrl + F2
--- Chess_game.py
+++ Chess_game.py
@@ -1,6 +1,101 @@
+# Import modules
 import numpy as np
+import pandas as pd 
 
-blk = 'x' 
-wht = 'o' 
 
-board = np.zeros((2,2)
+class init_board():
+    """Initliase dataframe to be used as board in the chess game.
+
+    Returns:
+        [dataframe] -- [the chess board]
+
+    """
+
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+
+    def create_board(self):
+        """[Create board from size specified].
+
+        Returns:
+            [df] -- [board]
+        """
+        np_array = np.zeros((self.rows, self.columns), dtype=np.int16)
+        df_array = pd.DataFrame(np_array)
+        return df_array
+
+    def rename_rows(self, dataframe):
+        """[rename columns tp letters].
+
+        Arguments:
+            dataframe {[pd.Dataframe]} -- [board/dataframe to be renamed]
+
+        Returns:
+            [type] -- [description]
+        """
+        renamed_board = dataframe.rename(columns={0: 'a', 1: 'b', 2: 'c',
+                                                  3: 'd', 4: 'e', 5: 'f', 
+                                                  6: 'g', 7: 'h'})
+        return renamed_board
+
+
+class init_pieces():
+    """[summary]
+    
+    Returns:
+        [type] -- [description]
+    """
+
+    def __init__(self, board_in):
+        self.board_in = board_in
+
+    def initiate_white(self):
+        self.board_in[0:2] = 1
+        return self.board_in
+
+    def initiate_black(self):
+        self.board_in[-2:] = 2
+        return self.board_in
+
+
+class make_move():
+    """Move the pieces using the coordinate system
+    
+    Returns:
+        [DataFrame] -- Update dataframe
+    """
+
+    def __init__(self, Board, x1, y1, x2, y2):
+        self.Board = Board
+        self.x1 = x1 
+        self.y1 = y1
+        self.x2 = x2 
+        self.y2 = y2
+
+    def clear_position(self):
+        self.Board[self.x1][self.y1] = 0
+        return init_board
+
+    def move_position(self):
+        self.Board[self.x2][self.y2] = 2
+        return init_board
+
+    
+brd = init_board(8, 8)
+init_board = brd.create_board()
+init_board = brd.rename_rows(init_board)
+
+pcs = init_pieces(init_board)
+init_board = pcs.initiate_white()
+init_board = pcs.initiate_black()
+
+mk_mv = make_move(init_board, 'g', 6, 'g', 5)
+init_board = mk_mv.clear_position()
+init_board = mk_mv.move_position()
+
+print(init_board) 
+
+var_row = input("Please enter row: ")
+var_col = input("Please enter column: ")
+print("You entered:{},{} ".format(var_row, var_col))
